4f0c0d01b05d5839b1d56e2dc94b539ab3fad019
felipesanderp <felipe.panisson@hotmail.com>
2024-02-05 12:24:11 -0300

    update breadcrumb

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -1,26 +1,42 @@
 import { Link, useLocation } from 'react-router-dom'
 
-export const Breadcrumb = () => {
+interface Breadcrumbs {
+  [key: string]: string
+}
+
+interface BreadcrumbProps {
+  breadcrumbs: Breadcrumbs
+}
+
+export const Breadcrumb = ({ breadcrumbs }: BreadcrumbProps) => {
   const location = useLocation()
   const pathnames = location.pathname.split('/').filter((x) => x)
+  const isHome = location.pathname === '/'
 
   return (
-    <div>
-      {pathnames.map((name, index) => {
-        const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
-        const isLast = index === pathnames.length - 1
-        return (
-          <span key={name}>
-            <Link
-              to={routeTo}
-              className={isLast ? 'font-bold' : 'text-blue-200'}
-            >
-              {name}
-            </Link>
-            {!isLast && <span className="mx-1">/</span>}
-          </span>
-        )
-      })}
+    <div className="">
+      <div className="flex">
+        <Link to="/" className="mr-1">
+          Home
+        </Link>
+        {!isHome && <span className="mx-1">{'>'}</span>}
+        {pathnames.map((name, index) => {
+          const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
+          const customName = breadcrumbs[routeTo]
+          const isLast = index === pathnames.length - 1
+          return (
+            <span key={name}>
+              {index > 0 && <span className="mx-1">{'>'}</span>}
+              <Link
+                to={routeTo}
+                className={isLast ? 'font-bold' : 'text-blue-200'}
+              >
+                {customName || name}
+              </Link>
+            </span>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,18 @@
+import { Breadcrumb } from './breadcrumb'
+
 export function Header() {
-  return <div className="flex h-16 bg-white border-b border-zinc-400"></div>
+  const breadcrumbs = {
+    '/': 'Home',
+    '/users': 'Usuários',
+    '/users/admins': 'Admins',
+    '/users/deliveryman': 'Entregadores',
+  }
+
+  return (
+    <div className="flex h-16 bg-white border-b border-zinc-400">
+      <div>
+        <Breadcrumb breadcrumbs={breadcrumbs} />
+      </div>
+    </div>
+  )
 }
