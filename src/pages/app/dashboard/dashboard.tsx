3ab5cb45b5ae169df78312e153edff3cf524c209
felipesanderp <felipe.panisson@hotmail.com>
2024-02-07 10:18:00 -0300

    month orders delivered card

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async'
 
 import { DayOrdersDeliveredCard } from './day-orders-delivered-card'
+import { MonthOrdersDeliveredCard } from './month-orders-delivered-card'
 
 export function Dashboard() {
   return (
@@ -13,6 +14,7 @@ export function Dashboard() {
 
         <div className="grid grid-cols-4 gap-4">
           <DayOrdersDeliveredCard />
+          <MonthOrdersDeliveredCard />
         </div>
       </div>
     </>
