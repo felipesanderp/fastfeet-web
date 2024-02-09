850f8bc160504b884cc4924431a448860d64a577
felipesanderp <felipe.panisson@hotmail.com>
2024-02-09 08:43:00 -0300

    logic to protect admin pages if cookie does not exists

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
 import { Sidebar } from '@/components/sidebar'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  const cookie = Cookies.get('fastfeet:token')
+
+  useEffect(() => {
+    if (cookie === undefined) {
+      navigate('/sign-in', { replace: true })
+    }
+  }, [cookie, navigate])
+
   return (
     <div className="min-h-screen lg:grid lg:grid-cols-app">
       <Sidebar />
