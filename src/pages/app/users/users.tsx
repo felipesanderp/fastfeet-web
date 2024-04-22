74f6d613e019675d6e2587c001017c49de28f8f8
felipesanderp <felipe.panisson@hotmail.com>
2024-04-22 13:48:37 -0300

    initial users table

--- src/pages/app/users/users.tsx
+++ src/pages/app/users/users.tsx
@@ -1,6 +1,7 @@
-import { File, ListFilter, PlusCircle } from 'lucide-react'
+import { File, ListFilter, MoreHorizontal, PlusCircle } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
+import { Badge } from '@/components/ui/badge'
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -21,10 +22,19 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export function Users() {
@@ -92,7 +102,69 @@ export function Users() {
                   <CardTitle>Usuários</CardTitle>
                   <CardDescription>Gerencie todos os usuários</CardDescription>
                 </CardHeader>
-                <CardContent></CardContent>
+                <CardContent>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="hidden w-[100px] sm:table-cell">
+                          <span className="sr-only">Image</span>
+                        </TableHead>
+                        <TableHead>Nome</TableHead>
+                        <TableHead>E-mail</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>
+                          <span className="sr-only">Actions</span>
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell className="hidden sm:table-cell">
+                          <img
+                            alt="User avatar"
+                            className="aspect-square rounded-md object-cover"
+                            height="64"
+                            width="64"
+                            src="https://github.com/diego3g.png"
+                          />
+                        </TableCell>
+                        <TableCell className="font-medium w-[350px]">
+                          Diego Fernandes
+                          <Badge className="ml-2" variant="outline">
+                            admin
+                          </Badge>
+                        </TableCell>
+                        <TableCell className="w-[320px]">
+                          diego3g@gmail.com
+                        </TableCell>
+                        <TableCell>
+                          <Badge className="bg-green-600 text-zinc-100">
+                            Ativo
+                          </Badge>
+                        </TableCell>
+                        <TableCell>
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button
+                                aria-haspopup="true"
+                                size="icon"
+                                variant="ghost"
+                              >
+                                <MoreHorizontal className="h-4 w-4" />
+                                <span className="sr-only">Toggle menu</span>
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                              <DropdownMenuItem>Edit</DropdownMenuItem>
+                              <DropdownMenuItem>Delete</DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </CardContent>
               </Card>
             </TabsContent>
           </Tabs>
