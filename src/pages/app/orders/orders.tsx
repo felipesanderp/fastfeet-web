753f8cd43d447bca557183779c3d09845eab55fc
felipesanderp <felipe.panisson@hotmail.com>
2024-04-25 11:00:39 -0300

    orders table base

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,4 +1,11 @@
-import { File, ListFilter, PlusCircle, Search } from 'lucide-react'
+import {
+  File,
+  ListFilter,
+  MoreHorizontal,
+  PlusCircle,
+  Search,
+  Truck,
+} from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 import {
@@ -21,8 +28,10 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
+  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import {
@@ -103,24 +112,129 @@ export function Orders() {
                   <CardDescription>Gerencie todos os pedidos</CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <Table>
-                    <TableHeader>
-                      <TableHead></TableHead>
-                      <TableHead></TableHead>
-                      <TableHead>Identificador</TableHead>
-                      <TableHead>Descrição</TableHead>
-                    </TableHeader>
-                    <TableBody>
-                      <TableRow>
-                        <TableCell>
-                          <Button variant="outline" size="xs">
-                            <Search className="size-3" />
-                            <span className="sr-only">Detalhes do pedido</span>
-                          </Button>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
+                  <div className="rounded-md border bg-muted/40">
+                    <Table>
+                      <TableHeader>
+                        <TableHead className="w-[64px]"></TableHead>
+                        <TableHead className="w-[285px]">
+                          Identificador
+                        </TableHead>
+                        <TableHead className="w-[440px]">Descrição</TableHead>
+                        <TableHead className="w-[220px]">
+                          Destinatário
+                        </TableHead>
+                        <TableHead className="w-[200px]">Status</TableHead>
+                        <TableHead className="w-[120px]"></TableHead>
+                      </TableHeader>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>
+                            <Button variant="outline" size="xs">
+                              <Search className="size-3" />
+                              <span className="sr-only">
+                                Detalhes do pedido
+                              </span>
+                            </Button>
+                          </TableCell>
+                          <TableCell className="font-mono text-xs font-medium">
+                            {crypto.randomUUID()}
+                          </TableCell>
+                          <TableCell className="truncate max-w-[440px]">
+                            50 caixas de chocolate ao leite com bastante
+                            cacauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+                          </TableCell>
+                          <TableCell>Carlos de Andrade</TableCell>
+                          <TableCell>
+                            <div className="flex items-center gap-2">
+                              <Truck className="size-4" />
+                              <p>Em entrega</p>
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  variant="ghost"
+                                  className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                                >
+                                  <MoreHorizontal className="h-4 w-4" />
+                                  <span className="sr-only">Open menu</span>
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent
+                                align="end"
+                                className="w-[160px]"
+                              >
+                                <DropdownMenuItem>Edit</DropdownMenuItem>
+                                <DropdownMenuItem>Make a copy</DropdownMenuItem>
+                                <DropdownMenuItem>Favorite</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                  Delete
+                                  <DropdownMenuShortcut>
+                                    ⌘⌫
+                                  </DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                          <TableCell>
+                            <Button variant="outline" size="xs">
+                              <Search className="size-3" />
+                              <span className="sr-only">
+                                Detalhes do pedido
+                              </span>
+                            </Button>
+                          </TableCell>
+                          <TableCell className="font-mono text-xs font-medium">
+                            {crypto.randomUUID()}
+                          </TableCell>
+                          <TableCell className="truncate max-w-[440px]">
+                            50 caixas de chocolate ao leite com bastante
+                            cacauuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+                          </TableCell>
+                          <TableCell>Carlos de Andrade</TableCell>
+                          <TableCell>
+                            <div className="flex items-center gap-2">
+                              <Truck className="size-4" />
+                              <p>Em entrega</p>
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  variant="ghost"
+                                  className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+                                >
+                                  <MoreHorizontal className="h-4 w-4" />
+                                  <span className="sr-only">Open menu</span>
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent
+                                align="end"
+                                className="w-[160px]"
+                              >
+                                <DropdownMenuItem>Edit</DropdownMenuItem>
+                                <DropdownMenuItem>Make a copy</DropdownMenuItem>
+                                <DropdownMenuItem>Favorite</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                  Delete
+                                  <DropdownMenuShortcut>
+                                    ⌘⌫
+                                  </DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </div>
                 </CardContent>
               </Card>
             </TabsContent>
